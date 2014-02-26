e8240057ccb6a511ebb819e207145bc71a6cbf76
jeremyBass <jeremybass@cableone.net>
2014-02-25 16:37:59 -0800

    the first example file, but temp only

---
+++ grunt/Gruntfile.js
@@ -0,0 +1,109 @@
+module.exports = function(grunt) {
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			styles: {
+				src: ['styles/skeleton.css','styles/colors.css','styles/spine.css','styles/respond.css'],
+				dest: 'build/<%= pkg.build_version %>/spine.css',
+			},
+			scripts: {
+				src: [
+					'scripts/debug.js',
+					'scripts/wsu_autocomplete.js',
+					'scripts/ui.spine.js',
+					'scripts/ui.spine.framework.js',
+					'scripts/ui.spine.search.js',
+					'scripts/ui.spine.social.js',
+					'scripts/ui.spine.analytics.js',
+					'scripts/spine.js'
+				],
+				dest: 'build/<%= pkg.build_version %>/spine.js',
+			},
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' +
+					'/*  See https://github.com/washingtonstateuniversity/WSU-spine/ for full source.*/\n'
+			},
+			build: {
+				src: 'build/<%= pkg.build_version %>/spine.js',
+				dest: 'build/<%= pkg.build_version %>/spine.min.js'
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					// Hmmm, in reverse order
+					'build/<%= pkg.build_version %>/spine.min.css': ['build/<%= pkg.build_version %>/spine.css']
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					{expand: true, dot:true, src: ['fonts/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['html/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, dot:true, src: ['icons/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['images/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['marks/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['scripts/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['styles/*'], dest: 'build/<%= pkg.build_version %>/'},
+					{expand: true, src: ['spine.html','spine.min.html','authors.txt','favicon.ico'], dest: 'build/<%= pkg.build_version %>/'},
+				]
+			}
+		},
+		jshint: {
+			files: ['Gruntfile.js', 'scripts/*.js'],
+			options: {
+				// options here to override JSHint defaults
+				boss: true,
+				curly: true,
+				eqeqeq: true,
+				eqnull: true,
+				expr: true,
+				immed: true,
+				noarg: true,
+				onevar: true,
+				quotmark: "double",
+				smarttabs: true,
+				trailing: true,
+				undef: true,
+				unused: true,
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true
+				}
+			}
+		},
+		preprocess : {
+			options: {
+				inline: true,
+				context : {
+					DEBUG: true,
+                    build_version : '<%= pkg.build_version %>'
+				}
+			},
+			js : {
+				src: 'build/<%= pkg.build_version %>/spine.js'
+			},
+            html : {
+                src : 'test/test.pre.html',
+                dest : 'test/test.html'
+            }
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-preprocess');
+
+	// Default task(s).
+	grunt.registerTask('default', ['concat','preprocess:js','cssmin','uglify','copy','preprocess:html']);
+};
---
+++ grunt/Readme.md
@@ -0,0 +1,1 @@
+At this time the grunt file is an example only from teh [WSU-spine github repo](https://github.com/washingtonstateuniversity/WSU-spine).
